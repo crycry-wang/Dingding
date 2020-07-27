aca374bbf00628e603a3d470fdf481674be137b1
Stark <comelydawn@gmail.com>
2020-07-28 01:36:12 +0800

    更新日曆1

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -9,7 +9,7 @@ let nowYear = today.getFullYear();
 
 // 往上個月
 
-function prevM(){
+function prevM() {
     let NowYear = (nowMonth === 0) ? nowYear - 1 : nowYear;
     let NowMonth = (nowMonth === 0) ? 11 : nowMonth - 1;
     showCalendar(NowMonth, NowYear);
@@ -18,7 +18,7 @@ function prevM(){
 
 // 往下個月
 
-function nextM(){
+function nextM() {
     let NowYear = (nowMonth === 11) ? nowYear + 1 : nowYear;
     let NowMonth = (nowMonth + 1) % 12;
     showCalendar(NowMonth, NowYear);
@@ -29,31 +29,30 @@ function nextM(){
 
 showCalendar(nowMonth, nowYear);
 
-function showCalendar(month, year){
-    
+function showCalendar(month, year) {
+
     // 當月第一天是周幾
     let firstDay = new Date(year, month).getDay();
     // console.log(firstDay); //3
-    
+
     // 如果一個月中有31天，則“第32天”將是下個月的1號。
     // 如果有30，則“第32天”將是下個月的2號。如果有28，則“第32天”將是下個月的4號。
     // 從32中減去任何一個，您就會得到正確的數字。
     let days = 32 - new Date(year, month, 32).getDate();
     // console.log(days); //31
-    
-    let tbl =document.getElementById("myCalendar");
-    let YandM = document.getElementById("YearAndMonth");
+
+    let tbl = document.getElementById("myCalendar");
 
     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    YandM.insertAdjacentHTML(`beforeend`, `<span id="yaM" class="mr-xs"></span><span id="YAM" class=""></span><span id="Yam" class="ml-xs"></span>`);
-    let titleyaM =document.getElementById("yaM");
-    let titleYAM =document.getElementById("YAM");
-    let titleYam =document.getElementById("Yam");
-    
-    yaM.innerHTML = [month+1]+ " " + months[month] + "";
-    YAM.innerHTML = "|" + "";
-    Yam.innerHTML = year;
+    // YandM.insertAdjacentHTML(`beforeend`, `<span id="yaM" class="mr-xs"></span><span id="YAM" class=""></span><span id="Yam" class="ml-xs"></span>`);
+    let numMonths = document.getElementById("myDates");
+    let engMonths = document.getElementById("myMonths");
+    let myYears = document.getElementById("myYears");
+
+    numMonths.innerHTML = [month + 1];
+    engMonths.innerHTML = months[month];
+    myYears.innerHTML = year;
 
     tbl.innerHTML = "";
 
@@ -62,37 +61,41 @@ function showCalendar(month, year){
     for (let i = 0; i < 6; i++) {
         let row = document.createElement("tr");
         for (let j = 0; j < 7; j++) {
-            
-            if(i === 0 && j < firstDay ){
-                let col =document.createElement("td");
+
+
+
+            if (i === 0 && j < firstDay) {
+                let col = document.createElement("td");
                 let cellText = document.createTextNode("");
                 col.appendChild(cellText);
                 row.appendChild(col);
-            }else if( i === 0 && j === firstDay){
-                let col =document.createElement("td");
-                let cellText = document.createTextNode(date);
-                col.appendChild(cellText);
-                row.appendChild(col);
-                date++;
-            }else if(date <= days){
+            } else if (i === 0 && j === firstDay) {
                 let col = document.createElement("td");
                 let cellText = document.createTextNode(date);
                 col.appendChild(cellText);
                 row.appendChild(col);
                 date++;
-            }else if(i === 5 && j === 0){
+            } else if (date <= days) {
+                let col = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                col.appendChild(cellText);
+                row.appendChild(col);
+                date++;
+            } else if (i === 5 && j === 0) {
                 break;
-            }else{
+            } else {
                 let col = document.createElement("td");
                 let cellText = document.createTextNode("");
                 col.appendChild(cellText);
                 row.appendChild(col);
                 // break;
             }
+
         }
 
         tbl.appendChild(row);
     }
+
 
 }
 
