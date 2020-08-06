9edf74726771406076f169eb6cde07581834cdd8
crycry <a22638456@gmail.com>
2020-08-06 20:41:46 +0800

    start

---
+++ routes/mSaveCoins.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../model/db');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('mSaveCoins');
+});
+
+module.exports = router;
---
+++ routes/sComment.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../model/db');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('sComment');
+});
+
+module.exports = router;
