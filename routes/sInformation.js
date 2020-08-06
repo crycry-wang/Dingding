6d2f781d9bd981f822dab36ae2fc03d177120d4b
Erh <teacafe333@gmail.com>
2020-08-07 00:49:04 +0800

    0807 12:49

--- routes/sInformation.js
+++ routes/sInformation.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../model/db');
-
+var district = ""
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    if (err) console.log("ERR!!");
-    db.query('SELECT * FROM `district`',function(err, results, fields){
+router.get('/', function (req, res, next) {
+
+    db.query('SELECT * FROM `district`', function (err, results, fields) {
+        if (err) console.log("ERR!!");
         district = JSON.stringify(results);
-        res.render('sInformation',{district});
+        res.render('sInformation', { district });
     })
-    
+
 });
 
 module.exports = router;
