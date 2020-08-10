484d393012ee14e47b5c7189d4d7c5c1492868c1
Erh <teacafe333@gmail.com>
2020-08-10 11:51:49 +0800

    0810 11:50

--- routes/sInformation.js
+++ routes/sInformation.js
@@ -6,12 +6,14 @@ var northDistrict = "";
 var storeInformation ="";
 var centralDistrict ="";
 var storeProduct="";
+var storeComment="";
+var comment="";
 let storeID = 1;
 let northDistrictSql = "SELECT * FROM `district` where area='北部'";
 let centralDistrictSql = "SELECT * FROM `district` where area='中部'";
 let storeInformationSql = 'SELECT * FROM `store` where storeID='+ storeID ;
 let storeProductSql = 'SELECT a.storeID,a.productID,a.productName,a.categoryID,a.productPhoto,a.productinformation,a.productPrice,b.categoryName FROM `product` a LEFT JOIN `category` b on a.categoryID=b.categoryID WHERE a.storeID='+ storeID;
-let commentSql = 'SELECT * FROM `comment` where storeID='+ storeID ;
+let commentSql = 'SELECT a.storeID,a.memberID,a.commentContent,a.commentScore,a.commentTime,b.memberName,b.memberPhoto FROM `comment` a LEFT JOIN `member` b on a.memberID=b.memberID where storeID='+ storeID ;
 let commentScoreSql = 'SELECT count(commentID) count,round(AVG(commentScore),1) star FROM `comment` WHERE storeID='+ storeID ;
 /* GET home page. */
 
@@ -78,19 +80,34 @@ const commentScoreData = (req) => {
         })
 })
 }
+
+//店家評論
+const commentData = (req) => {
+  return new Promise((resolve,reject) => {
+    db.queryAsync(commentSql)
+        .then(results => {
+            resolve(results);
+        })
+        .catch(ex => {
+            reject(ex);
+        })
+})
+}
 router.get('/', async (req, res) => {
     const a = await storeInformationData(req);
     const b = await getNorthDistrictData(req);
     const c = await getCentralDistrictData(req);
     const d = await storeProductData(req);
     const e = await commentScoreData(req);
+    const f = await commentData(req);
     storeInformation = JSON.stringify(a)
     northDistrict = JSON.stringify(b)
     centralDistrict = JSON.stringify(c)
     storeProduct = JSON.stringify(d)
-    storeProduct = JSON.stringify(e)
+    storeComment = JSON.stringify(e)
+    comment = JSON.stringify(f)
     // console.log(northDistrict)
-    res.render('sInformation', { a: storeInformation ,b:northDistrict,c:centralDistrict,d:storeProduct,e:storeProduct })
+    res.render('sInformation', { a: storeInformation ,b:northDistrict,c:centralDistrict,d:storeProduct,e:storeComment,f:comment })
 })
 
 
