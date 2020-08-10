784c7c7c0784d76df0028913fbe0e93aa3302a9f
crycry <a22638456@gmail.com>
2020-08-10 20:41:59 +0800

    更改db

--- model/db.js
+++ model/db.js
@@ -4,11 +4,20 @@ const bluebird = require('bluebird');
 var db = mysql.createConnection({
    host: 'localhost',
    user: 'root',
-   password: 'root',
+   password: '',
    database: 'dingding',
-   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+   // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
    dateStrings : true
 });
+ 
+// var db = mysql.createConnection({
+//    host: 'localhost',
+//    user: 'root',
+//    password: 'root',
+//    database: 'dingding',
+//    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+//    dateStrings : true
+// });
  
 db.connect(function(err) {
    if (err) { console.log("connERR!!!") };
