277b81ae00f1affb2afef7b3aa8892024d3c8e2e
crycry <a22638456@gmail.com>
2020-08-10 13:21:30 +0800

    日曆暫存

--- routes/mCalendar.js
+++ routes/mCalendar.js
@@ -3,8 +3,36 @@ var router = express.Router();
 var db = require('../model/db');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('mCalendar', {active: 'mCalendar'});
+
+// 會員
+const memberId = 38;
+const memberSelect = 'select * from `member` where memberID=';
+const member = memberSelect + memberId;
+
+const getMemberData = (req) => {
+  return new Promise((resolve, reject) => {
+      db.queryAsync(memberId)
+          .then(results => {
+              resolve(results);
+          })
+          .catch(ex => {
+              reject(ex);
+          });
+  })
+};
+
+
+//傳資料到表單裡
+router.get('/', async (req, res) => {
+  newsJSON = JSON.stringify(await getMemberData(req));
+  
+  
+  res.render('mCalendar', { 
+      mMemberData: newsJSON,
+      active: 'mCalendar'});
+
 });
+
+
 
 module.exports = router;
