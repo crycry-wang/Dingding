d0a50b94479c8b9faaeafbcab0dc2885dd521026
Erh <teacafe333@gmail.com>
2020-08-10 15:22:19 +0800

    pick crycry 15:22

--- routes/mCalendar.js
+++ routes/mCalendar.js
@@ -11,11 +11,7 @@ const member = memberSelect + memberId;
 
 const getMemberData = (req) => {
   return new Promise((resolve, reject) => {
-<<<<<<< HEAD
       db.queryAsync(member)
-=======
-      db.queryAsync(memberId)
->>>>>>> 277b81a... 日曆暫存
           .then(results => {
               resolve(results);
           })
@@ -30,10 +26,6 @@ const getMemberData = (req) => {
 router.get('/', async (req, res) => {
   newsJSON = JSON.stringify(await getMemberData(req));
   
-<<<<<<< HEAD
-=======
-  
->>>>>>> 277b81a... 日曆暫存
   res.render('mCalendar', { 
       mMemberData: newsJSON,
       active: 'mCalendar'});
