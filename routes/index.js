839be84152b561d63bb2f36baf76bfee686b26b7
Erh <teacafe333@gmail.com>
2020-08-10 15:05:18 +0800

    pick erh 15:05

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,88 @@ var express = require('express');
 var router = express.Router();
 var db = require('../model/db');
 
+//進到首頁的店家照片+店名
+let storeIndexSql = 'SELECT storeID,storeName,storeBanner FROM `store`';
+//地區
+let districtSql = 'SELECT * FROM `district`';
+// 點擊店家後連結店家資訊
+let storeSql = 'SELECT a.storeID,a.storeName,a.storeBanner,a.storeIntroduction,a.storePhone,a.storeAddress,a.storeClose,a.storeOpen,a.storeNotice,b.productID,b.productName,b.categoryID,b.productPhoto,b.productInformation,b.productPrice,c.commentID,c.memberID,c.commentContent,c.commentScore,c.commentTime,d.categoryName,e.districtID,f.districtName,f.country,g.memberName FROM `store` a JOIN `product` b on a.storeID=b.storeID JOIN `comment` c on a.storeID=c.storeID JOIN `category` d on b.categoryID=d.categoryID JOIN `delivery` e on a.storeID=e.storeID JOIN `district` f on e.districtID=f.districtID JOIN `member` g on c.memberID=g.memberID where a.storeID=1;';
+//首頁店家評分
+let commentSql = 'SELECT storeID,count(commentID) count,round(AVG(commentScore),1) star FROM `comment` group by storeID';
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+// router.post('/',(req,res)=>{
+//   const rSql = 'insert into `member`(`eMail`, `password`, `memberName`,`memberPhone`) VALUES (?,?,?,?)'
+//   db.queryAsync(rSql, [
+//     req.body.registeredEmail,
+//     req.body.registeredPassword,
+//     req.body.registeredName,
+//     req.body.registeredPhone,
+// ])
+// .then(results=>{
+//   res.json(results);
+// })
+// .catch(ex=>{
+//   console.log('ex:', ex);
+// })
+// })
+
+router.post('/',(req,res)=>{
+  db.query('SELECT eMail FROM `member` WHERE eMail=?',[req.body.registeredEmail],(error,results) => {
+    console.log(req.body.registeredEmail)
+    console.log(results)
+    if(error){
+      console.log(error);
+    }
+    else if(results.length > 0){
+      console.log("123456456")
+      return res.send('/',{message:'信箱已被使用'});
+    }else{
+      
+    }
+  })
+
+  const rSql = 'insert into `member`(`eMail`, `password`, `memberName`,`memberPhone`) VALUES (?,?,?,?)'
+  db.queryAsync(rSql, [
+    req.body.registeredEmail,
+    req.body.registeredPassword,
+    req.body.registeredName,
+    req.body.registeredPhone,
+],(error,results) => {
+  if(error){
+    console.log(error)
+  } else {
+    console.log(results)
+    return res.send('/',{message:'您已成功註冊'});
+  }
+})
+})
+
+
+// router.get('/', function (req, res, next) {
+  
+//   db.query(districtSql, function (err, results, fields) {
+//     if (err) console.log("ERR!!");
+//     district = JSON.stringify(results);
+//     // console.log(district)
+//     return district;
+
+//   });
+//   db.query(storeSql, function (err, results, fields) {
+//     if (err) console.log("ERR!!");
+//     store = JSON.stringify(results);
+//     console.log(store)
+//     return store;
+
+//   });
+
+
+//   res.render('index', { a: district, b: store });
+// });
 
 module.exports = router;
--- routes/sInformation.js
+++ routes/sInformation.js
@@ -1,6 +1,97 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../model/db');
+const { resolve, reject } = require('bluebird');
+var northDistrict = "";
+var storeInformation ="";
+var centralDistrict ="";
+var storeProduct="";
+let storeID = 1;
+let northDistrictSql = "SELECT * FROM `district` where area='北部'";
+let centralDistrictSql = "SELECT * FROM `district` where area='中部'";
+let storeInformationSql = 'SELECT * FROM `store` where storeID='+ storeID ;
+let storeProductSql = 'SELECT a.storeID,a.productID,a.productName,a.categoryID,a.productPhoto,a.productinformation,a.productPrice,b.categoryName FROM `product` a LEFT JOIN `category` b on a.categoryID=b.categoryID WHERE a.storeID='+ storeID;
+let commentSql = 'SELECT * FROM `comment` where storeID='+ storeID ;
+let commentScoreSql = 'SELECT count(commentID) count,round(AVG(commentScore),1) star FROM `comment` WHERE storeID='+ storeID ;
+/* GET home page. */
+
+
+
+// 地區
+// 北部
+const getNorthDistrictData = (req)=>{
+    return new Promise((resolve,reject)=>{
+      db.queryAsync(northDistrictSql)
+        .then(results=>{
+          resolve(results);
+        })
+        .catch(ex=>{
+          reject(ex);
+        })
+    })
+  }
+//中部
+const getCentralDistrictData = (req)=>{
+    return new Promise((resolve,reject)=>{
+      db.queryAsync(centralDistrictSql)
+        .then(results=>{
+          resolve(results);
+        })
+        .catch(ex=>{
+          reject(ex);
+        })
+    })
+  }
+//店家資訊
+const storeInformationData = (req) => {
+    return new Promise((resolve, reject) => {
+        db.queryAsync(storeInformationSql)
+            .then(results => {
+                resolve(results);
+            })
+            .catch(ex => {
+                reject(ex);
+            })
+    })
+}
+//店家菜單
+const storeProductData = (req) => {
+    return new Promise((resolve,reject) => {
+        db.queryAsync(storeProductSql)
+            .then(results => {
+                resolve(results);
+            })
+            .catch(ex => {
+                reject(ex);
+            })
+    })
+}
+//店家評分
+const commentScoreData = (req) => {
+  return new Promise((resolve,reject) => {
+    db.queryAsync(commentScoreSql)
+        .then(results => {
+            resolve(results);
+        })
+        .catch(ex => {
+            reject(ex);
+        })
+})
+}
+router.get('/', async (req, res) => {
+    const a = await storeInformationData(req);
+    const b = await getNorthDistrictData(req);
+    const c = await getCentralDistrictData(req);
+    const d = await storeProductData(req);
+    const e = await commentScoreData(req);
+    storeInformation = JSON.stringify(a)
+    northDistrict = JSON.stringify(b)
+    centralDistrict = JSON.stringify(c)
+    storeProduct = JSON.stringify(d)
+    storeProduct = JSON.stringify(e)
+    // console.log(northDistrict)
+    res.render('sInformation', { a: storeInformation ,b:northDistrict,c:centralDistrict,d:storeProduct,e:storeProduct })
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
