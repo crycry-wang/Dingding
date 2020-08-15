50401d8e7ab2d83fbe7d5a46b5fabbbb141149a6
crycry <a22638456@gmail.com>
2020-08-15 19:37:49 +0800

    統計餐點完成

--- routes/sStat.js
+++ routes/sStat.js
@@ -9,27 +9,42 @@ let storeID;
 let store;
 
 // 類別訂單量
- let orderSum;
+let dateStart;
+let dateEnd;
+let orderSum;
 
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     storeID = 4;
     store = 'select a.`storeID`,a.`storeName`,\
     a.`storePhoto`,count(b.`noticeStatus`) as\
      noticeCount from `store` as a,`notice` as\
       b where a.`storeID`=b.`toWhoID` and b.`toWhoType`=1\
        and b.`noticeStatus`=1 and storeID=' + storeID;
-    
+
     // 類別訂單量
-     orderSum='SELECT a.orderID,sum(b.`quality`) total,\
-     c.`categoryID`,d.`categoryName` FROM `order` as a,\
-     `orderdetail` as b,`product` as c,`category` as d WHERE \
-     a.orderID=b.orderID and b.`productID`=c.`productID` and \
-     c.`categoryID`=d.`categoryID` and a.`orderStatus`=5  and \
-     a.storeID='+storeID+' GROUP by c.`categoryID`';
-    
-     next();
+    dateStart = '"2020-06-01"';
+    dateEnd = '"2020-08-31"';
+
+    allType='SELECT a.`categoryName`,b.productName,a.`categoryID` FROM `category` as a,\
+    `product` as b where a.categoryID=b.categoryID and a.storeId='+ storeID;
+
+    orderSum = 'SELECT d.`categoryName`,c.productName,c.`categoryID`,sum(b.`quality`) total\
+      FROM `order` as a, `orderdetail` as b,`product` as c,\
+     `category` as d WHERE a.orderID=b.orderID and b.`productID`=c.`productID`\
+      and c.`categoryID`=d.`categoryID` and a.`orderStatus`=5 and a.storeID='+ storeID +
+        ' and a.`orderDeadline`>= ' + dateStart + ' and a.`orderDeadline`< ' + dateEnd +
+        'GROUP by c.productName'
+    console.log(orderSum);
+
+
+
+
+    next();
+
+
+
 
 });
 
@@ -55,18 +70,31 @@ const getOrderSum = (req) => {
             });
     })
 };
+const getallType = (req) => {
+    return new Promise((resolve, reject) => {
+        db.queryAsync(allType)
+            .then(results => {
+                resolve(results);
+            })
+            .catch(ex => {
+                reject(ex);
+            });
+    })
+};
 
-    router.get('/', async (req, res) => {
-        newsJSON = JSON.stringify(await getStoreData(req));
-        newsJSON1 = JSON.stringify(await getOrderSum(req));
+router.get('/', async (req, res) => {
+    newsJSON = JSON.stringify(await getStoreData(req));
+    newsJSON1 = JSON.stringify(await getOrderSum(req));
+    newsJSON2 = JSON.stringify(await getallType(req));
 
-        res.render('sStat', {
-            storeData: newsJSON,
-            orderSumData: newsJSON1,
-            active: 'sStat'
-        });
+    res.render('sStat', {
+        storeData: newsJSON,
+        orderSumData: newsJSON1,
+        allTypeData: newsJSON2,
+        active: 'sStat'
     });
-    
+});
+
 
 
 module.exports = router;
